4a5f94ba18e9a7d6a305b6a91985934ff8a096fb
nfgoto <gotoflorian.pro@gmail.com>
2019-02-12 19:08:50 +0100

    add more interactivity

--- singleton/index.js
+++ singleton/index.js
@@ -4,19 +4,23 @@ const Shopper = require('./Shopper');
 
 logger.log('App loading...');
 
-const Florian = new Shopper('Florian', '123456789');
+const florian = new Shopper('Florian', '123456789');
 const tech_shop = new Store('Go2store - Future Tech Now', [{
-        item: 'Super Quantum Calculator',
-        qty: 6,
-        price: '356,998'
-    },
-    {
-        item: 'Quantum Satellite Phone',
-        qty: 14,
-        price: '31,998'
-    }
+    item: 'Super Quantum Calculator',
+    qty: 6,
+    price: '356998'
+},
+{
+    item: 'Quantum Satellite Phone',
+    qty: 14,
+    price: '31998'
+}
 ]);
 
 logger.log('... Finished Configuration.');
 
-console.log(`${logger.count} logs total.`)
+logger.log(`${florian.name} bought a ${tech_shop.inventory[1].item}`);
+
+logger.log(`He now has ${florian.money - tech_shop.inventory[1].price} money`);
+
+logger.log(`${logger.count} logs total.`);
